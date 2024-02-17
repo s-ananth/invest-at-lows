830f6112477db0a911dc78c23fe2add35c0a14ff
s-ananth <s.ananth55@gmail.com>
2024-02-17 16:16:53 -0500

    Check backtest
    Added ability to check back test results on tickers currently near lows

--- code/investAtLows.py
+++ code/investAtLows.py
@@ -2,9 +2,23 @@ import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
 import math
+pd.set_option('display.max_columns', None)
 
-def tickers_close_to_low(csv_file_path, start_date, tkrList,
-                         priceTol, minPeriod):
+def bactest_results_on_current_low_tickers(tkrList, resPath):
+    sortCol = 'win%'
+    keyCols = ['ticker', 'SLFlag', 'holdingPeriod',
+               'NumAll', 'win%', 'avgRet%', 'maxW%', 'maxL%']
+    roundCols = ['win%', 'avgRet%', 'maxW%', 'maxL%']
+    resDF = pd.read_excel(resPath)
+    resDF = resDF[keyCols] # reorder DF
+    resDF[roundCols] = resDF[roundCols].round(4)
+    for tkr in tkrList:
+        cur = resDF[resDF['ticker'] == tkr]
+        print('\n')
+        print(cur.sort_values(by=sortCol, ascending=False).head(3))
+
+
+def tickers_close_to_low(csv_file_path, start_date, tkrList, priceTol, minPeriod):
     breachedList = []
     notYetBreachedList = []
     for tkr in tkrList:
@@ -37,27 +51,32 @@ def tickers_close_to_low(csv_file_path, start_date, tkrList,
         res = pd.DataFrame(data_filled)
         res.to_excel(fn)
         print('Lists saved to ' + fn)
+    return breachedList, notYetBreachedList
 
-def calculate_summary_stats(trades, startInv):
+def calculate_summary_stats(trades, startInv, SLFlag):
 
     summaryStats = {}
     trades['return'] = (trades['sellPrc'] - trades['buyPrc']) / trades['buyPrc']
 
     trades['winning_trade'] = trades['return'] > 0
     trades['losing_trade'] = trades['return'] <= 0
+    summaryStats['SLFlag'] = SLFlag
 
-    summaryStats['winPct'] = (trades['return'] > 0).mean()
-    summaryStats['avgWinReturn'] = trades.loc[trades['return'] > 0, 'return'].mean()
-    summaryStats['avgLostReturn'] = trades.loc[trades['return'] < 0, 'return'].mean()
-    summaryStats['maxLostTrdRet'] = trades.loc[trades['return'] < 0, 'return'].min()
-    summaryStats['maxWinTrdRet'] = trades.loc[trades['return'] > 0, 'return'].max()
+    summaryStats['win%'] = (trades['return'] > 0).mean()
+    summaryStats['avgRet%'] = trades['return'].mean()
+
+    summaryStats['avgWRet%'] = trades.loc[trades['return'] > 0, 'return'].mean()
+    summaryStats['avgLRet%'] = trades.loc[trades['return'] <= 0, 'return'].mean()
+
+    summaryStats['maxL%'] = trades.loc[trades['return'] <= 0, 'return'].min()
+    summaryStats['maxW%'] = trades.loc[trades['return'] > 0, 'return'].max()
 
     num_winning_trades = trades['winning_trade'].sum()
     num_losing_trades = trades['losing_trade'].sum()
 
-    summaryStats['NumWinTrades'] = num_winning_trades
-    summaryStats['NumLoseTrades'] = num_losing_trades
-    summaryStats['TotalTrades'] = num_winning_trades + num_losing_trades
+    summaryStats['NumW'] = num_winning_trades
+    summaryStats['NumL'] = num_losing_trades
+    summaryStats['NumAll'] = num_winning_trades + num_losing_trades
 
     # For this strategy which has long periods of no trades per ticker when the price has already
     # bounced off of the low, simple cumulative return is a better proxy rather than geometric return
@@ -80,6 +99,9 @@ def shares_transacted(buyPrc, sellPrc, curPortVal):
     newPortVal = curPortVal + shares * (sellPrc - buyPrc)
     return shares, newPortVal
 
+def set_close_to_end_of_holding_period():
+    pass
+
 def backtest_strategy(csv_file, minPeriod, stock, holding_period,
                       start_date, startInv,
                       SLFlag=False, SLPer=0.01):
@@ -88,9 +110,8 @@ def backtest_strategy(csv_file, minPeriod, stock, holding_period,
     df['Date'] = pd.to_datetime(df['Date'])  # Make sure the 'Date' column is in datetime format
     df = df[df['Date'] >= start_date] # Limit trading to desired period
     df.reset_index(drop=True, inplace=True)
-    df['52_Week_Low'] = df['Close'].rolling(window=minPeriod, min_periods=minPeriod).min()
-    buy_signals = df[df['Close'] == df['52_Week_Low']]
-    df['buy_signals'] = (df['Close'] == df['52_Week_Low']).astype(int)
+    df['Period_Low'] = df['Close'].rolling(window=minPeriod, min_periods=minPeriod).min()
+    df['buy_signals'] = (df['Close'] == df['Period_Low']).astype(int)
 
     fn = '../results/' + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '_Res_' + stock + '_' + str(holding_period) + '_Data.xlsx'
     df.to_excel(fn)
@@ -104,6 +125,7 @@ def backtest_strategy(csv_file, minPeriod, stock, holding_period,
     except:
         print('No initial low found for ticker ' + stock)
         return None, None
+
     ttlDays = len(listClose)
 
     resDct = {}
@@ -116,11 +138,29 @@ def backtest_strategy(csv_file, minPeriod, stock, holding_period,
         buyPrc = listClose[curIdx]
         resDct[trdCtr]['buyPrc'] = buyPrc   # Buy at/near close, this is an approximation since you might not be able
                                             # to perfectly transact at the close price
-        resDct[trdCtr]['sellDate'] = listDate[curIdx + holding_period]
-        sellPrc = listClose[curIdx + holding_period]
-        resDct[trdCtr]['sellPrc'] = sellPrc
-
-        resDct[trdCtr]['retPerTrade'] = (sellPrc - buyPrc) / buyPrc
+        if SLFlag: # check for early SL up to end of holding period
+            slPrc = buyPrc * (1 - SLPer)
+            slPeriod = listClose[curIdx: curIdx + holding_period] # Does not include the last day of the holding period
+                                                                    # because that is handled below. Only for early SL
+            try: # Close early
+                slIdx = next(i for i, num in enumerate(slPeriod) if num < slPrc)
+                resDct[trdCtr]['sellDate'] = listDate[curIdx + slIdx]
+                sellPrc = listClose[curIdx + slIdx]
+                resDct[trdCtr]['sellPrc'] = sellPrc
+                resDct[trdCtr]['retPerTrade'] = (sellPrc - buyPrc) / buyPrc
+                resDct[trdCtr]['SLClose'] = 1
+            except: # Close at end of holding period
+                resDct[trdCtr]['sellDate'] = listDate[curIdx + holding_period]
+                sellPrc = listClose[curIdx + holding_period]
+                resDct[trdCtr]['sellPrc'] = sellPrc
+                resDct[trdCtr]['retPerTrade'] = (sellPrc - buyPrc) / buyPrc
+                resDct[trdCtr]['SLClose'] = 0
+        else:
+            resDct[trdCtr]['sellDate'] = listDate[curIdx + holding_period]
+            sellPrc = listClose[curIdx + holding_period]
+            resDct[trdCtr]['sellPrc'] = sellPrc
+            resDct[trdCtr]['retPerTrade'] = (sellPrc - buyPrc) / buyPrc
+            resDct[trdCtr]['SLClose'] = 0
 
         # Update index to value after previous closing day and when the next breach of the period low occurs
         # Another break may not occur in the remaining period, hence the need for the error handling
@@ -131,8 +171,8 @@ def backtest_strategy(csv_file, minPeriod, stock, holding_period,
             #nextBreachIndex = listSignal[idxUpToHoldPerPlus1:].index(1) + idxUpToHoldPerPlus1
 
             # Method 2 - multiple trades during period near lows
-            idxUpToHoldPerPlus1 = curIdx + 1
-            nextBreachIndex = listSignal[idxUpToHoldPerPlus1:].index(1) + curIdx + 1
+            idxNext = curIdx + 1 # Can be consecutive days
+            nextBreachIndex = listSignal[idxNext:].index(1) + curIdx + 1
 
             curIdx = nextBreachIndex # Update curIdx
             trdCtr += 1
@@ -145,7 +185,7 @@ def backtest_strategy(csv_file, minPeriod, stock, holding_period,
     if trades.empty: # case where there low is found too close to the end of the dataset, the while loop is not entered
         return None, None
     else:
-        fn = '../results/' + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '_Res_' + stock + '_' + str(holding_period) + '.xlsx'
+        fn = '../results/' + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '_Res_' + stock + '_' + str(holding_period) + '_SLFlag=' + str(SLFlag) + '.xlsx'
         trades.to_excel(fn)
-        stats = calculate_summary_stats(trades, startInv)
+        stats = calculate_summary_stats(trades, startInv, SLFlag)
     return trades, stats
--- code/main.py
+++ code/main.py
@@ -1,63 +1,53 @@
-from investAtLows import backtest_strategy, tickers_close_to_low
+from investAtLows import *
 import pandas as pd
+from itertools import product
 from datetime import datetime, date
 import sys
+sys.path.append("../..")
+from utils.utils import tsx60, top100FromSP500
 
-# Yahoo tickers of stocks in TSX 60
-tsx60 = ['ABX.TO','AEM.TO','AQN.TO','ATD.TO','BAM.TO','BCE.TO','BIP-UN.TO','BMO.TO','BN.TO','BNS.TO','CAE.TO',
-         'CAR-UN.TO','CCL-B.TO','CCO.TO','CM.TO','CNQ.TO','CNR.TO','CP.TO','CSU.TO','CTC-A.TO','CVE.TO','DOL.TO',
-         'EMA.TO','ENB.TO','RM.TO','FNV.TO','FSV.TO','FTS.TO','GIB-A.TO','GILTO','H.TO','IFC.TO','IMO.TO','K.TO',
-         'L.TO','MFC.TO','MG.TO','MRU.TO','NA.TO','NTR.TO','OTEX.TO','POW.TO','PPL.TO','QSR.TO','RCI-B.TO','RY.TO',
-         'SAP.TO','SHOP.TO','SLF.TO','SU.TO','T.TO','TD.TO','TECK-B.TO','TOU.TO','TRI.TO','TRP.TO','WCN.TO','WN.TO',
-         'WPM.TO','WSP.TO']
+runBacktestPerTicker = False
+runCheckCurrentLow = True
+flags = [runBacktestPerTicker, runCheckCurrentLow]
+assert sum(flags) == 1, 'Only one run type can be True'
 
-# Top 100 stocks by weight in the S&P 500 - https://www.slickcharts.com/sp500 - Updated 12/29/2023
-top100FromSP500 = ['AAPL','MSFT','AMZN','NVDA','GOOGL','META','TSLA','GOOG','BRK.B','AVGO','JPM','UNH','LLY','V','XOM',
-                   'JNJ','MA','HD','PG','COST','MRK','ABBV','ADBE','CVX','CRM','AMD','BAC','PEP','KO','WMT','ACN','NFLX',
-                   'INTC','MCD','TMO','CSCO','LIN','ABT','WFC','CMCSA','INTU','ORCL','DIS','QCOM','PFE','VZ','TXN','AMGN',
-                   'DHR','CAT','UNP','IBM','BA','PM','NOW','SPGI','COP','GE','HON','AMAT','NKE','UBER','LOW','GS','NEE',
-                   'PLD','BKNG','RTX','ISRG','T','MS','BLK','UPS','MDT','ELV','SBUX','AXP','DE','TJX','VRTX','LRCX','BMY',
-                   'SCHW','CVS','AMT','SYK','GILD','ADI','LMT','C','MDLZ','ETN','ADP','MU','BX','REGN','MMC','PANW','PGR',
-                   'CB']
-
+# Strategy Input Variables
+data_path = '../data/'
+results_path = '../results/'
+results_fn = '2024-02-17_15-07-36_Summary.xlsx' # Most recent full run results
 allTkrs = tsx60 + top100FromSP500
-
-flagCheckCurrentLow = True
-
-csv_file_path = '../data/'
-stocks_to_test = allTkrs
-start_date = datetime(2018,1,1)
+tkrs_to_test = allTkrs
+start_date = datetime(2019,1,1)
 holding_period_days = [5, 10, 15, 20]  # Adjust the holding period as needed
 minPeriod = 252 # in days, 252 business days is approx a year
 startInv = 10000
-stopLossFlag = True
-stopLossPer = 0.01
+SLFlagList = [True, False]
+stopLossPer = 0.05
 
-# Input for checking tickers close to low
-if flagCheckCurrentLow:
-    priceTol = 0.05
-    tickers_close_to_low(csv_file_path, start_date, stocks_to_test,
-                         priceTol, minPeriod)
+if runCheckCurrentLow: # Check if tickers are close to low
+    backTestResFile = ''
+    priceTol = 0.02
+    breached, notBreached = tickers_close_to_low(data_path, start_date, tkrs_to_test, priceTol, minPeriod)
+    bactest_results_on_current_low_tickers(breached + notBreached, results_path + results_fn)
 
-else: # Run backtest
+else: # Run Strategy
     summaryDF = pd.DataFrame()
+    numRuns = len(list(product(holding_period_days, tkrs_to_test, SLFlagList)))
+    ctr = 1
     for h in holding_period_days:
-        print('\nRunning holding period ' + str(h))
-        for stock in stocks_to_test:
-            print('Running ticker ' + stock)
-            fn = csv_file_path + stock + '.csv'
-            trades, stats = backtest_strategy(fn, minPeriod, stock, h,
-                                              start_date, startInv,
-                                              stopLossFlag, stopLossPer)
-            if trades is not None:
-                stats['stock'] = stock
-                stats['holdingPeriod'] = h
-                summaryDF = summaryDF._append(stats, ignore_index=True)
+        #print('\nRunning holding period ' + str(h))
+        for tkr in tkrs_to_test:
+            for slFlag in SLFlagList:
+                #print('Running ticker ' + tkr)
+                print('Running ' + str(ctr) + ' of ' + str(numRuns))
+                fn = data_path + tkr + '.csv'
+                trades, stats = backtest_strategy(fn, minPeriod, tkr, h, start_date, startInv, slFlag, stopLossPer)
+                if trades is not None:
+                    stats['ticker'] = tkr
+                    stats['holdingPeriod'] = h
+                    summaryDF = summaryDF._append(stats, ignore_index=True)
+                ctr += 1
 
     fn = '../results/' + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '_Summary.xlsx'
     summaryDF.sort_values('AnnReturn', ascending=False, inplace=True)
     summaryDF.to_excel(fn)
-
-
-
-
